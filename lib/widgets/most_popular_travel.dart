ea8121e203d339dbec4da73f2a967d83f838a4de
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-12-09 15:56:01 +0600

    minor changes

--- lib/widgets/most_popular_travel.dart
+++ lib/widgets/most_popular_travel.dart
@@ -13,27 +13,25 @@ class _Most_Popular_Travel_ListState extends State<Most_Popular_Travel> {
   var travelList = Travel.getMostTravelList();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.separated(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            var travel = travelList[index];
-            return Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(travel.travelImg,
-                      width: 150.0, height: 300.0, fit: BoxFit.cover),
-                )
-              ],
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Container(
-              width: 10,
-            );
-          },
-          itemCount: travelList.length),
-    );
+    return ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          var travel = travelList[index];
+          return Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(travel.travelImg,
+                    width: 150.0, height: 300.0, fit: BoxFit.cover),
+              )
+            ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Container(
+            width: 10,
+          );
+        },
+        itemCount: travelList.length);
   }
 }
--- lib/widgets/travel_info.dart
+++ lib/widgets/travel_info.dart
@@ -13,61 +13,59 @@ class _Travel_infoState extends State<Travel_info> {
   var travel = Travel.getTravelList();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView.builder(
-          itemCount: travel.length,
-          itemBuilder: (context, index) {
-            return Stack(
-              fit: StackFit.expand,
-              alignment: AlignmentDirectional.bottomCenter,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(travel[index].travelImg,
-                      width: MediaQuery.of(context).size.width * .98,
-                      height: 480,
-                      fit: BoxFit.cover),
-                ),
-                Positioned(
-                    bottom: 20.0,
-                    left: 20.0,
-                    child: Column(
-                      children: [
-                        Text(
-                          travel[index].travelName,
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          travel[index].travelDesc,
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ],
-                    )),
-                Positioned(
-                    bottom: -25,
-                    right: 20,
-                    child: Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(50)),
-                      child: const Center(
-                        child: Icon(
-                          Icons.arrow_right_alt_outlined,
-                          color: Colors.white,
-                        ),
+    return PageView.builder(
+        itemCount: travel.length,
+        itemBuilder: (context, index) {
+          return Stack(
+            fit: StackFit.expand,
+            alignment: AlignmentDirectional.bottomCenter,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(travel[index].travelImg,
+                    width: MediaQuery.of(context).size.width * .98,
+                    height: 480,
+                    fit: BoxFit.cover),
+              ),
+              Positioned(
+                  bottom: 20.0,
+                  left: 20.0,
+                  child: Column(
+                    children: [
+                      Text(
+                        travel[index].travelName,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
                       ),
-                    ))
-              ],
-            );
-          }),
-    );
+                      Text(
+                        travel[index].travelDesc,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  )),
+              Positioned(
+                  bottom: -25,
+                  right: 20,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Center(
+                      child: Icon(
+                        Icons.arrow_right_alt_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ))
+            ],
+          );
+        });
   }
 }
